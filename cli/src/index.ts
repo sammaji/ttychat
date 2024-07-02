092a3eac898db71f00e8fcb05b1cdfa2858dadb6
samyabrata-maji <samyabratamaji334@gmail.com>
2024-07-03 00:45:27 +0530

    feat: add envs to dockerfile + npmignore file

--- cli/src/index.ts
+++ cli/src/index.ts
@@ -6,7 +6,10 @@ import dotenv from "dotenv";
 
 dotenv.config()
 
-const socket = io(process.env.SOCKET_URL);
+const socketUrl = process.env.NODE_ENV === "production" ? process.env.SOCKET_URL : process.env.SOCKET_DEV_URL;
+if (!socketUrl) throw new Error("SOCKET_URL or SOCKET_DEV_URL env variable is not set");
+
+const socket = io(socketUrl);
 
 type Log = { message: string };
 
